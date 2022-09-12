19149d990ddd07aa9052fda38dfba1848858e30d
Yong Xin <mycodegenesis@gmail.com>
2022-09-12 17:37:11 +0000

    commit tree 1fb2033c050099e887b00b2c25ef1bdc61ad9d8e

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+    
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+
+const newArray = [...cats.slice(0), "Broom"];
+function appendCat(name) {
+    return newArray;
+}
+
+const newArr = ["Arnold", ...cats.slice(0)];
+function prependCat(name) {
+    return newArr;
+}
+
+const newAr = [...cats.slice(0, -1)];
+function removeLastCat() {
+    return newAr;
+}
+
+const newA = [...cats.slice(1)];
+function removeFirstCat() {
+    return newA;
+}
